e21f5763e2db4654bac5e72857b753b351119280
EdwBit <73597471+edwbit@users.noreply.github.com>
2025-04-06 23:26:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ if "selected_model" not in st.session_state:
 
 models = {
     "deepseek-r1-distill-llama-70b": {"name": "deepseek-r1-distill-llama-70b", "tokens": 16384},
-    "meta-llama/llama-4-maverick-17b-128e-instruct": {"name": "meta-llama/llama-4-maverick-17b-128e-instruct", "tokens": 131072},   
+    "meta-llama/llama-4-scout-17b-16e-instruct: {"name": "meta-llama/llama-4-scout-17b-16e-instruct", "tokens": 131072},   
 }
 
 # Layout for model selection and max token slider
